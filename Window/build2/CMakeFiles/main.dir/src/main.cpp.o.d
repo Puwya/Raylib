7c1c3ce3389c1b2e1f998ba83cf6d91f962a60fb
Carlos Aguilera <18aguilerac@gmail.com>
2022-05-13 12:28:04 -0700

    init

---
+++ Window/build2/CMakeFiles/main.dir/src/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/main.dir/src/main.cpp.o: \
+  /Users/ca/CC/RayLib/Window/src/main.cpp \
+  /Users/ca/CC/RayLib/Window/src/../build/_deps/raylib-src/src/raylib.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/13.0.0/include/stdarg.h
